d324bf21ce53f06339e1fe6ab3e58f055721f209
Timbre Freeman <180427@cvsd356.org>
2018-09-25 14:44:41 -0700

    clean up the public page for the api

--- node_server/public_page/sketch.js
+++ node_server/public_page/sketch.js
@@ -1,19 +1,8 @@
 function setup() {
   noCanvas();
-  //createCanvas(400,400);
-  //background(51);
+
   loadJSON('/list/get', gotData);
-
-  //var button = select('#add');
-  //button.mousePressed(addByButton);
-  var buttonPost = select('#postAdd');
-  buttonPost.mousePressed(addByButtonPost);
-  var buttonPostRemove = select('#postRemove');
-  buttonPostRemove.mousePressed(removeByButtonPost);
 }
-
-/*function draw() {
-}*/
 
 function gotData(data) {
   createP("<br><br>Bad Items:");
@@ -25,49 +14,17 @@ function gotData(data) {
     createP(data[i][1]);
   }
 }
-/*
-function addByButton() {
-  var keyIn = select('#key').value();
-  var badIn = select('#bad').value();
-  var goodIn = select('#good').value();
-  console.log(keyIn, badIn, goodIn);
 
-  loadJSON('/list/add/' + keyIn + '/' + badIn + '/' + goodIn, function (dataBack) {
-    /*if(dataBack.meg == "Access Granted"){
-      loadJSON('/list/get', gotDataUpdate);
-    }else {
-      alert(dataBack.meg);
-    }*//*
-    console.log(dataBack);
-  });
-}*/
-
-function addByButtonPost() {
+function byButtonPost(url) {
   var data = {
+    name: select('#name').value(),
     key: select('#key').value(),
     badItme: select('#bad').value(),
     goodItme: select('#good').value()
   }
-  console.log(data.keyIn, data.badIn, data.goodIn);
+  console.log(data.name, data.key, data.badItme, data.goodItme);
 
-  httpPost('/list/add/', 'json', data, callBackFun, errorFun);
-  function callBackFun(result) {
-    console.log(result);
-  }
-  function errorFun(err) {
-    console.log(err);
-  }
-}
-
-function removeByButtonPost() {
-  var data = {
-    key: select('#key').value(),
-    badItme: select('#bad').value(),
-    goodItme: select('#good').value()
-  }
-  console.log(data.keyIn, data.badIn, data.goodIn);
-
-  httpPost('/list/remove/', 'json', data, callBackFun, errorFun);
+  httpPost(url, 'json', data, callBackFun, errorFun);
   function callBackFun(result) {
     console.log(result);
   }
